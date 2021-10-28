44fb239642a65e7a44d1062cdc9c4fa7bf7c1fda
Kishan Changela <52081694+kpchangela@users.noreply.github.com>
2021-10-28 12:24:45 -0400

    Added mouseout event and fixed year

--- scripts/meteorite_globe_vis_drag.js
+++ scripts/meteorite_globe_vis_drag.js
@@ -173,9 +173,17 @@ function drawMarkers() {
             document.getElementById("meteorite_name").innerHTML = "Meteorite Name: " + d.name;
             document.getElementById("found_or_fell").innerHTML = "Found/Fell: " + d.fall;
             document.getElementById("mass").innerHTML = "Mass: " + d.mass;
-            document.getElementById("date_found").innerHTML = "Year: " + d.year;
+            document.getElementById("date_found").innerHTML = "Year: " + d.year.slice(0,4);
             document.getElementById("lat").innerHTML = "Latitude: " + d.reclat;
             document.getElementById("long").innerHTML = "Longitude: " + d.reclong;
+        })
+        .on("mouseout", function(event, d) {
+            document.getElementById("meteorite_name").innerHTML = "Meteorite Name: ";
+            document.getElementById("found_or_fell").innerHTML = "Found/Fell: ";
+            document.getElementById("mass").innerHTML = "Mass: ";
+            document.getElementById("date_found").innerHTML = "Year: ";
+            document.getElementById("lat").innerHTML = "Latitude: ";
+            document.getElementById("long").innerHTML = "Longitude: ";
         });
 
     markerGroup.each(function () {
