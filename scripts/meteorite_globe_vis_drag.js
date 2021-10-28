bba732374f589905e2933749bbdf39e9ec226742
Kishan Changela <52081694+kpchangela@users.noreply.github.com>
2021-10-28 11:47:29 -0400

    did some stuff
    added info for data

--- scripts/meteorite_globe_vis_drag.js
+++ scripts/meteorite_globe_vis_drag.js
@@ -143,7 +143,15 @@ function drawMarkers() {
             gdistance = d3.geoDistance(coordinate, projection.invert(center));
             return gdistance > 1.57 ? "none" : "steelblue";
         })
-        .attr("r", 5);
+        .attr("r", 5)
+        .on("mouseover", function(event, d) {
+            document.getElementById("meteorite_name").innerHTML = "Meteorite Name: " + d.name;
+            document.getElementById("found_or_fell").innerHTML = "Found/Fell: " + d.fall;
+            document.getElementById("mass").innerHTML = "Mass: " + d.mass;
+            document.getElementById("date_found").innerHTML = "Year: " + d.year;
+            document.getElementById("lat").innerHTML = "Latitude: " + d.reclat;
+            document.getElementById("long").innerHTML = "Longitude: " + d.reclong;
+        });
 
     markerGroup.each(function () {
         this.parentNode.appendChild(this);
