a600ab226b7f3ec663674bb035a5fbb0075eeb24
lukebrazinski <lukebraz12@gmail.com>
2021-10-29 14:00:21 -0400

    basic framework for plot data joins

--- scripts/plots.js
+++ scripts/plots.js
@@ -1,8 +1,9 @@
-let plotwidth = 350;
-let plotheight = 350;
+let plotwidth = 65;
+let plotheight = 65;
 let margin_x = 20;
 let margin_y = 20;
 
+let data = "./data/meteoriteexample.json"
 
 
 class Plot {
@@ -10,32 +11,65 @@ class Plot {
         this.svg = svg_elem;
     }
     render() {
-         
-        // Add a background rectangle
+        //Fetch Json
+        json = dataFetch();
+        
+        //Linear scales
+        let myx = d3.scaleLinear()
+			.domain([861, 2012])
+			.range([0, width]);
+
+		let myy = d3.scaleLinear()
+			.domain([0, 60000000])
+			.range([height, 0]);
+        
+        // Add rectangles for each plot
         this.svg.append("rect")
-            .attr("class", "plot")
-            .attr("x", -320)
+            .attr("class", "massyearplot")
+            .attr("x", 18)
             .attr("y", 0)
             .attr("width", plotwidth)
             .attr("height", plotheight);
 
         this.svg.append("rect")
-            .attr("class", "plot")
-            .attr("x", -320)
-            .attr("y", 500)
+            .attr("class", "massdensplot")
+            .attr("x", 18)
+            .attr("y", plotheight+margin_y)
             .attr("width", plotwidth)
             .attr("height", plotheight);
+
+        this.svg.append("rect")
+            .attr("class", "yeardensplot")
+            .attr("x", 18)
+            .attr("y", 2*plotheight+2*margin_y)
+            .attr("width", plotwidth)
+            .attr("height", plotheight);
+
+        // MassYear Scatter Plot data join
+        let massyearplot = this.svg.select('massyearplot').data(json, function(d) {return d});
+        massyearplot.enter().append('circle')
+            .attr('class', 'scatterdot')
+            .attr("cx", function(d) { return myx(d[0]['year']); })
+            .attr("cy", function(d) { return myy(d[0]['mass']); })
+            .attr("r", 1);
+        
     }
 }
-
-producePlots();
+let json = [];
+async function dataFetch() {
+    let response = await fetch(data);
+    let json = await response.json();
+    console.log('look at me', json[0]['year'])
+    return json;
+}
 producePlots();
 function producePlots() {
     const svg = d3.select('#plots_svg')
-        .attr("width", plotwidth + 2*margin_x)
-        .attr("height", plotheight + 2*margin_y);
-
+        //.attr("width", plotwidth + 2*margin_x)
+        //.attr("height", plotheight + 2*margin_y)
+        
             
     let vis = new Plot(svg);
+    //d3.json(data).then(json => {vis.render(json);});
     vis.render();
 } 
