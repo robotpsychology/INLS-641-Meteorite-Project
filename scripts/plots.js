e670c7168477f369d451650397f32d88556da3c8
lukebrazinski <lukebraz12@gmail.com>
2021-11-10 19:39:27 -0500

    updated svg, loaded data correctly, updated rectangles

--- scripts/plots.js
+++ scripts/plots.js
@@ -1,19 +1,17 @@
-let plotwidth = 65;
-let plotheight = 65;
-let margin_x = 20;
-let margin_y = 20;
-
-
 class Plot {
     constructor(svg_elem) {
         this.svg = svg_elem;
         this.data = "/data/meteoriteexample.json";
         this.loadAndPrepare();
     }
-    render() {
+    render(d) {
         //Fetch Json
         //json = dataFetch();
-                
+        let plotwidth = 200;
+        let plotheight = 200;
+        let margin_x = 30;
+        let margin_y = 50;
+        let plotspacing = 10;
         //Linear scales for mass year scatter plot
         let myx = d3.scaleLinear()
 			.domain([861, 2012])
@@ -24,12 +22,30 @@ class Plot {
 			.range([height, 0]);
         
         // Add rectangles for each plot
+        let plot_groups = this.svg.append('g')
+            .data(this.data)
+            .attr('transform', 'translate('+margin_x+','+margin_y+')');
+
+
+        plot_groups.append('rect')
+            .attr('class', 'massyearplot')
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", plotwidth)
+            .attr("height", plotheight)
+            .append("text")
+                .attr("x", 0)
+                .attr("y", 0)
+                .attr("text-anchor", "middle")
+                .attr("dominant-baseline", "hanging")
+                .text("Mass(g) vs. Year");
+
 
         //Mass vs. Year Scatter
-        this.svg.append("rect")
-            .attr("class", "massyearplot")
-            .attr("x", 18)
-            .attr("y", 0)
+        plot_groups.append("rect")
+            .attr("class", "massdensplot")
+            .attr("x", 0)
+            .attr("y", plotheight+margin_y)
             .attr("width", plotwidth)
             .attr("height", plotheight)
             .append("text")
@@ -40,28 +56,19 @@ class Plot {
                 .text("Mass(g) vs. Year");
 
         //Mass density plot
-        this.svg.append("rect")
-            .attr("class", "massdensplot")
-            .attr("x", 18)
-            .attr("y", plotheight+margin_y)
-            .attr("width", plotwidth)
-            .attr("height", plotheight);
-
-        // Year Density Plot
-        this.svg.append("rect")
+        plot_groups.append("rect")
             .attr("class", "yeardensplot")
-            .attr("x", 18)
+            .attr("x", 0)
             .attr("y", 2*plotheight+2*margin_y)
             .attr("width", plotwidth)
             .attr("height", plotheight);
 
+        console.group('hi',d);
+        let massyearplot = plot_groups.select('massyearplot').data(d, function(d) {return d.id;});
         
-        // MassYear Scatter Plot data join
-        let massyearplot = this.svg.select('massyearplot').data(this.data, function(d) {return d.id;});
-
         massyearplot.enter().append('circle')
             .attr('class', 'scatterdot')
-            .attr("cx", function(d) { return myx(d.year); })
+            .attr("cx", function(d) { return myx(d.year.slice(0, 4)); })
             .attr("cy", function(d) { return myy(d.mass); })
             .attr("r", 1);
 
@@ -72,25 +79,24 @@ class Plot {
         let thisvis = this;
     
         //Load data from json
-        
-        d3.json(this.data, function(d) {
-            console.log(d);           
-        }).then(function(data) {
-            let min_mass = d3.min(data, function(d) {return d.mass;});
-            let max_mass = d3.max(data, function(d) {return d.mass;});
-            let min_year = d3.min(data, function(d) {return d.year.slice(0, 4);});
-            let max_year = d3.max(data, function(d) {return d.year.slice(0, 4);});
-        })
+        d3.json(this.data).then(function(d) {
+            console.log('howdy',d);
+            let data = d;
+            thisvis.render(d);
+            return d;
+        });
+            
+            
     }
 }
 //dataFetch function to use json
-let json = [];
-async function dataFetch() {
-    let response = await fetch(data);
-    let json = await response.json();
-    console.log('look at me', json[0]['year'])
-    return json;
-}
+// let json = [];
+// async function dataFetch() {
+//     let response = await fetch(data);
+//     let json = await response.json();
+//     console.log('look at me', json[0]['year'])
+//     return json;
+// }
 
 
 //Produce plots and call render function
@@ -100,5 +106,6 @@ function producePlots() {
     const svg = d3.select('#plots_svg')      
             
     let vis = new Plot(svg);
-    vis.render();
+    vis.loadAndPrepare();
+    //vis.render();
 } 
