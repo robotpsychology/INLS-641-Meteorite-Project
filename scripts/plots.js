5eccc60f8f8f1300bfcf4c837c55d466a87cbaec
robotpsychology <42260976+robotpsychology@users.noreply.github.com>
2021-11-23 12:23:49 -0500

    Fixed yearless checbox error.

--- scripts/plots.js
+++ scripts/plots.js
@@ -11,13 +11,13 @@ class Plot {
 
         // Add rectangles for each plot
         this.massyearplot = this.svg.append('g')
-            .attr('transform', 'translate('+this.margin_x+','+this.margin_y+')');
+            .attr('transform', 'translate(' + this.margin_x + ',' + this.margin_y + ')');
 
         this.massdensplot = this.svg.append('g')
-            .attr('transform', 'translate('+this.margin_x+','+this.margin_y+')');
+            .attr('transform', 'translate(' + this.margin_x + ',' + this.margin_y + ')');
 
         this.yeardensplot = this.svg.append('g')
-            .attr('transform', 'translate('+this.margin_x+','+this.margin_y+')');
+            .attr('transform', 'translate(' + this.margin_x + ',' + this.margin_y + ')');
 
         //Mass vs. year plot
         this.massyearplot.append('rect')
@@ -26,12 +26,12 @@ class Plot {
             .attr("y", 0)
             .attr("width", this.plotwidth)
             .attr("height", this.plotheight);
-        
+
         //Mass Dens Scatter
         this.massdensplot.append("rect")
             .attr("class", "massdensplot")
             .attr("x", 0)
-            .attr("y", this.plotheight+this.margin_y)
+            .attr("y", this.plotheight + this.margin_y)
             .attr("width", this.plotwidth)
             .attr("height", this.plotheight);
 
@@ -39,52 +39,52 @@ class Plot {
         this.yeardensplot.append("rect")
             .attr("class", "yeardensplot")
             .attr("x", 0)
-            .attr("y", 2*this.plotheight+2*this.margin_y)
+            .attr("y", 2 * this.plotheight + 2 * this.margin_y)
             .attr("width", this.plotwidth)
             .attr("height", this.plotheight);
 
-        
+
         //Add Axes Labels
         // Add the x axis labels.
-    	this.massyearplot.append("text")
-            .attr("x",this.plotwidth/2)
-            .attr("y",this.plotheight +3)
+        this.massyearplot.append("text")
+            .attr("x", this.plotwidth / 2)
+            .attr("y", this.plotheight + 3)
             .attr("dominant-baseline", "hanging")
             .attr("text-anchor", "middle")
             .text("Year");
 
         this.massdensplot.append("text")
-            .attr("x",this.plotwidth/2)
-            .attr("y",2*this.plotheight + this.margin_y+3)
+            .attr("x", this.plotwidth / 2)
+            .attr("y", 2 * this.plotheight + this.margin_y + 3)
             .attr("dominant-baseline", "hanging")
             .attr("text-anchor", "middle")
             .text("Mass(g)");
 
         this.yeardensplot.append("text")
-            .attr("x",this.plotwidth/2)
-            .attr("y",3*this.plotheight + 2*this.margin_y+3)
+            .attr("x", this.plotwidth / 2)
+            .attr("y", 3 * this.plotheight + 2 * this.margin_y + 3)
             .attr("dominant-baseline", "hanging")
             .attr("text-anchor", "middle")
             .text("Year");
 
         // Add y axis labels
         this.massyearplot.append("text")
-            .attr("x",-this.plotwidth/2 - this.margin_y/2)
-            .attr("y",-15)
+            .attr("x", -this.plotwidth / 2 - this.margin_y / 2)
+            .attr("y", -15)
             .attr("dominant-baseline", "hanging")
             .attr("transform", "rotate(270,0,0)")
             .text("Mass(g)");
 
         this.massdensplot.append("text")
-            .attr("x",-2*this.plotwidth)
-            .attr("y",-15)
+            .attr("x", -2 * this.plotwidth)
+            .attr("y", -15)
             .attr("dominant-baseline", "hanging")
             .attr("transform", "rotate(270,0,0)")
             .text("Num. Meteorites");
 
         this.yeardensplot.append("text")
-            .attr("x",-3*this.plotwidth - this.margin_y)
-            .attr("y",-15)
+            .attr("x", -3 * this.plotwidth - this.margin_y)
+            .attr("y", -15)
             .attr("dominant-baseline", "hanging")
             .attr("transform", "rotate(270,0,0)")
             .text("Num. Meteorites");
@@ -92,35 +92,35 @@ class Plot {
 
         // this.loadAndPrepare();
 
-        
+
     }
     render(data) {
-        
+
         //Check data
-        console.log('hi',data);
-        console.log('yo', document.getElementById("max_year").value);
+        // console.log('hi',data);
+        // console.log('yo', document.getElementById("max_year").value);
 
         //Linear scales for mass year scatter plot
         let myx = d3.scaleLinear()
-		 	.domain([Number(document.getElementById("min_year").value), Number(document.getElementById("max_year").value)])
-		 	.range([0, this.plotwidth]);
+            .domain([Number(document.getElementById("min_year").value), Number(document.getElementById("max_year").value)])
+            .range([0, this.plotwidth]);
 
-		let myy = d3.scaleLinear()
-		 	.domain([Number(document.getElementById("min_mass").value), Number(document.getElementById("max_mass").value)])
-		 	.range([this.plotheight, 0]);
+        let myy = d3.scaleLinear()
+            .domain([Number(document.getElementById("min_mass").value), Number(document.getElementById("max_mass").value)])
+            .range([this.plotheight, 0]);
 
         //mass axes labels
         this.massyearplot.append("text")
-            .attr("x",-this.plotwidth - this.margin_y/2+25)
-            .attr("y",-15)
+            .attr("x", -this.plotwidth - this.margin_y / 2 + 25)
+            .attr("y", -15)
             .attr("dominant-baseline", "hanging")
             .attr("transform", "rotate(270,0,0)")
             .style("font-size", "12")
             .text(document.getElementById("min_mass").value);
 
         this.massyearplot.append("text")
-            .attr("x",this.margin_y/2-50)
-            .attr("y",-15)
+            .attr("x", this.margin_y / 2 - 50)
+            .attr("y", -15)
             .attr("dominant-baseline", "hanging")
             .attr("transform", "rotate(270,0,0)")
             .style("font-size", "12")
@@ -128,57 +128,57 @@ class Plot {
 
         //year axes labels
         this.massyearplot.append("text")
-            .attr("x",this.plotwidth)
-            .attr("y",this.plotheight +3)
+            .attr("x", this.plotwidth)
+            .attr("y", this.plotheight + 3)
             .attr("dominant-baseline", "hanging")
             .attr("text-anchor", "middle")
             .style("font-size", "12")
             .text(document.getElementById("max_year").value);
 
         this.massyearplot.append("text")
-            .attr("x",0)
-            .attr("y",this.plotheight +3)
+            .attr("x", 0)
+            .attr("y", this.plotheight + 3)
             .attr("dominant-baseline", "hanging")
             .attr("text-anchor", "middle")
             .style("font-size", "12")
             .text(document.getElementById("min_year").value);
-        
+
         //datajoin for massyear plot
-        let massyear = this.massyearplot.selectAll('.scatterdot').data(data, function(d) {return d.id;});
+        let massyear = this.massyearplot.selectAll('.scatterdot').data(data, function (d) { return d.id; });
 
         //add circles
         massyear.exit()
-                .attr("r",0)
-                .remove();
+            .attr("r", 0)
+            .remove();
 
         massyear.enter().append('circle')
             .attr('class', 'scatterdot')
-            .attr("cx", function(d) {
+            .attr("cx", function (d) {
                 if (d.year && Number(d.year.slice(0, 4)) <= Number(document.getElementById("max_year").value)) {
                     return myx(Number(d.year.slice(0, 4)));
                 }
                 else {
                     return -1000;
-                } 
-                 })
-            .attr("cy", function(d) { return myy(Number(d.mass)); })
+                }
+            })
+            .attr("cy", function (d) { return myy(Number(d.mass)); })
             .attr("r", 2);
-        
 
-        
+
+
     }
     //Load data and call render
     loadAndPrepare() {
         let thisvis = this;
-    
+
         //Load data from json
-        d3.json(this.data).then(function(data) {
-            
+        d3.json(this.data).then(function (data) {
+
             thisvis.render(data);
-            
+
         });
-            
-            
+
+
     }
 }
 
@@ -186,8 +186,8 @@ class Plot {
 //Produce plots and call load function
 producePlots();
 function producePlots() {
-    const svg = d3.select('#plots_svg')      
-            
+    const svg = d3.select('#plots_svg')
+
     let vis = new Plot(svg);
     vis.loadAndPrepare();
-} 
+}
